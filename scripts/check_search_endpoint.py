b7a3d0242ad6b9166965bcb3d7f432acde1fceeb
maxachis <maxachis@gmail.com>
2024-11-24 11:58:36 -0500

    Adapt to v2 and refactor

--- scripts/check_search_endpoint.py
+++ scripts/check_search_endpoint.py
@@ -9,48 +9,53 @@ from logging_logic import setup_logger
 from constants import BASE_URL
 from webhook_logic import DiscordPoster, SMSNotifier
 
+ENDPOINT_NAME = "Typeahead"
 
-def check_search_endpoint(webhook_url: str):
-    discord_poster = DiscordPoster(webhook_url)
-    sms_notifier = SMSNotifier()
-    logger = setup_logger()
+class Notifier:
+    def __init__(self, webhook_url: str, notify=True):
+        self.webhook_url = webhook_url
+        self.logger = setup_logger()
+        self.notify = notify
+
+    def handle_error(self, msg: str):
+        self.logger.error(msg)
+        if self.notify:
+            DiscordPoster(self.webhook_url).post_to_discord(msg)
+            SMSNotifier().send_sms(msg)
+
+    def info(self, msg: str):
+        self.logger.info(msg)
+
+
+def check_search_endpoint(webhook_url: str, notify=True):
+    notifier = Notifier(webhook_url=webhook_url, notify=notify)
     try:
         response = requests.get(
             url=get_url(),
-            params=get_search_query()
         )
         response.raise_for_status()  # Check for HTTP errors
-
-        data = response.json()["data"]
-        if len(data) > 0:
-            logger.info("Search endpoint is functioning as expected.")
-        else:
-            msg = "Search endpoint returned an empty response."
-            logger.error(msg)
-            discord_poster.post_to_discord(msg)
-            sms_notifier.send_sms(msg)
+        data = response.json()["suggestions"]
+        evaluate_response(data, notifier)
     except Exception as e:
-        msg = f"Search endpoint check failed: {e}"
-        logger.error(msg)
-        discord_poster.post_to_discord(msg)
-        sms_notifier.send_sms(msg)
+        msg = f"{ENDPOINT_NAME} endpoint check failed: {e}"
+        notifier.handle_error(msg)
+
+
+def evaluate_response(data, notifier):
+    if len(data) > 0:
+        notifier.info(f"{ENDPOINT_NAME} endpoint is functioning as expected.")
+    else:
+        msg = f"{ENDPOINT_NAME} endpoint returned an empty response."
+        notifier.handle_error(msg)
 
 
 def get_url():
-    url = f"{BASE_URL}/search-tokens"
+    url = f"{BASE_URL}/typeahead/locations?query=Pitt"
     return url
 
-
-def get_search_query():
-    query = {
-        "arg1": "stops",
-        "arg2": "pittsburgh",
-        "endpoint": "quick-search"
-    }
-    return query
 
 
 if __name__ == "__main__":
     dotenv.load_dotenv()
     webhook_url = os.getenv("WEBHOOK_URL")
-    check_search_endpoint(webhook_url)
+    check_search_endpoint(webhook_url, notify=False)
